b461a8162cb7161e08a19e54b38cedbbb27ec080
Bhargavi <bhagi.bhat6@gmail.com>
2026-03-05 11:08:30 -0800

    Add PWA icons and favicon

---
+++ sw.js
@@ -0,0 +1,32 @@
+const CACHE = 'dots-boxes-v1';
+const FILES = [
+  './',
+  './index.html',
+  './style.css',
+  './script.js',
+  './gameLogic.js',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png',
+  './favicon.ico'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(cache => cache.addAll(FILES))
+  );
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(cached => cached || fetch(e.request))
+  );
+});
